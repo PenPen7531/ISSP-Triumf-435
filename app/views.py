74640e7d57d8f7d8ba6fc447c4ca309414605954
Jeff7531 <jeffmkwang@gmail.com>
2022-10-13 08:56:33 -0700

    Added new HTML page that contains dynamic site for mobile

--- app/views.py
+++ app/views.py
@@ -3,4 +3,9 @@ from flask import render_template
 
 @app.route("/")
 def index():
+    return render_template("public/mobile_index.html")
+
+
+@app.route("/webpage")
+def home():
     return render_template("public/index.html")
