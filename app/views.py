9051712531f5d5ae9a779c11416220eeed49129e
Jeff7531 <jeffmkwang@gmail.com>
2022-10-13 14:24:42 -0700

    Added new routes to python flask. Added new dashboard page with no dynamic features yet

--- app/views.py
+++ app/views.py
@@ -1,11 +1,22 @@
 from app import app
 from flask import render_template
+from flask import Flask, request, jsonify, render_template, redirect, session
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("public/mobile_index.html")
+    print(request.form.get("password"))
+    if request.method=="GET":
+        return render_template("public/index.html")
 
+    if request.method=="POST":
+        if request.form.get("password")=="Test":
+            return redirect("/view")
+    return redirect("/error")
 
-@app.route("/webpage")
+@app.route("/view", methods=["GET", "POST"])
 def home():
-    return render_template("public/index.html")
+    if request.method=="GET":
+        return render_template("public/dash.html")
+    if request.method=="POST":
+        dashboard=request.form.get("dashboard")
+        return redirect(f"/dashboard/{dashboard}")
