169def9785a09b3b69b4f3ee6da87240a128692e
Jake Martin <jm9357481@gmail.com>
2022-10-13 20:55:47 -0700

    remove unused imports

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from app import app
 from flask import render_template
-from flask import Flask, request, jsonify, render_template, redirect, session
-
+from flask import request, jsonify, redirect, session
 import requests
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,6 +14,7 @@ def index():
         if request.form.get("password")=="Test":
             return redirect("/dashboard")
     return redirect("/error")
+
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def home():
@@ -33,6 +34,7 @@ def dashboard(dashboard):
     if dashboard=="test":
         return render_template("/public/test_dash.html")
 
+
 # will have to install pip dependencies again from inside virtual env
 # as I added requests
 # pip install -r requirements.txt
@@ -42,6 +44,7 @@ def get_jaya(pv_list):
     r = requests.post(url_full, json=data)
     jsondata = r.json()
     return jsondata
+
 
 @app.route("/try_this", methods=["GET", "POST"])
 def try_this():
@@ -68,3 +71,4 @@ def try_this():
         jaya_json = get_jaya(pv_list)
 
         return jaya_json
+
