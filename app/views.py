a1b1ad20df08c6f3e74a45b3faea74c76bcd4bd2
Jeff7531 <jeffmkwang@gmail.com>
2022-10-19 23:58:27 -0700

    Added an error page if the user does not sign in. Added logout function and sessions for flask API.

--- app/views.py
+++ app/views.py
@@ -1,38 +1,46 @@
 from app import app
-from flask import render_template
-from flask import request, jsonify, redirect, session
+from flask import render_template, request, jsonify, redirect, session
 import requests
+
+app.secret_key="session_key666"
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    print(request.form.get("password"))
+    
     if request.method=="GET":
+        session['user']=None
         return render_template("public/index.html")
 
     if request.method=="POST":
         if request.form.get("password")=="Test":
+            session['user']='yes'
             return redirect("/dashboard")
-    return redirect("/error")
+    return redirect("/public/error")
 
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def home():
-    if request.method=="GET":
-        return render_template("public/dash.html")
-    if request.method=="POST":
-        
-        dashboard=request.form.get("dashboard")
-        if dashboard==None:
-            return render_template("public/dash.html")
-        else:
-            return redirect(f"/view/{dashboard}")
+    if session["user"]:
+        if request.method=="GET":
+            return render_template("/public/dash.html")
+        if request.method=="POST":
+            
+            dashboard=request.form.get("dashboard")
+            if dashboard==None:
+                return render_template("public/dash.html")
+            else:
+                return redirect(f"/view/{dashboard}")
+    return render_template('/public/error.html')
 
 
 @app.route("/view/<dashboard>", methods=["GET"])
 def dashboard(dashboard):
-    if dashboard=="test":
-        return render_template("/public/test_dash.html")
+    if session['user']:
+        if dashboard=="test":
+            return render_template("/public/test_dash.html")
+    return render_template('/public/error.html')
 
 
 # will have to install pip dependencies again from inside virtual env
@@ -48,27 +56,28 @@ def get_jaya(pv_list):
 
 @app.route("/try_this", methods=["GET", "POST"])
 def try_this():
+    if session['user']:
+        if request.method == "GET":
+            return render_template("/public/submit_pv.html")
 
-    if request.method == "GET":
-        return render_template("/public/submit_pv.html")
+        # pv_list = [
+        #         'IOS:MB:MASSOVERQ2', 'IOS:MB:MASSOVERQ.INPA',
+        #         'IOS:XCB1AW:STATON', 'IOS:B1A:POS:STATON',
+        #         'IOS:XCB1AW:RDVOL', 'IOS:B1A:POS:RDVOL',
+        #         'IOS:PSWXCB1A:STATON', 'MCIS:BIAS0:STATON',
+        #         'MCIS:BIAS0:RDVOL', 'IOS:BIAS:STATON',
+        #         'IOS:BIAS:RDVOL', 'IOS:FC3:SCALECUR',
+        #         'IOS:FC3:STATOFF', 'IOS:FC6:SCALECUR',
+        #         'IOS:FC6:STATOFF',
+        #         ]
 
-    # pv_list = [
-    #         'IOS:MB:MASSOVERQ2', 'IOS:MB:MASSOVERQ.INPA',
-    #         'IOS:XCB1AW:STATON', 'IOS:B1A:POS:STATON',
-    #         'IOS:XCB1AW:RDVOL', 'IOS:B1A:POS:RDVOL',
-    #         'IOS:PSWXCB1A:STATON', 'MCIS:BIAS0:STATON',
-    #         'MCIS:BIAS0:RDVOL', 'IOS:BIAS:STATON',
-    #         'IOS:BIAS:RDVOL', 'IOS:FC3:SCALECUR',
-    #         'IOS:FC3:STATOFF', 'IOS:FC6:SCALECUR',
-    #         'IOS:FC6:STATOFF',
-    #         ]
+        if request.method == "POST":
+            pv_from_form = request.form
+            pv_list = []
+            pv_list.append(pv_from_form['address'])
 
-    if request.method == "POST":
-        pv_from_form = request.form
-        pv_list = []
-        pv_list.append(pv_from_form['address'])
+            jaya_json = get_jaya(pv_list)
 
-        jaya_json = get_jaya(pv_list)
-
-        return jaya_json
+            return jaya_json
+    return render_template('/public/error.html')
 
