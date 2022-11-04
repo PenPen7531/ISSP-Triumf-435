b2cc3c90934806abdf1ef3a8fa758ab69a22d0d1
Jake Martin <jm9357481@gmail.com>
2022-11-03 18:11:45 -0700

    add route to rename dashboards

--- app/views.py
+++ app/views.py
@@ -85,6 +85,25 @@ def delete_dash(dash_name):
             return redirect ("/dashboard")
 
 
+@app.route("/dashboard/rename/<dname>", methods=['GET', 'POST'])
+def rename_dash(dname):
+    """
+    Rename Dashboard
+
+    Use OS import to rename JSON file of dashboard.
+    """
+    if session['user']:
+        if request.method == 'GET':
+            return render_template('/public/rename.html')
+        if request.method == 'POST':
+            dash_file = os.path.join("dashboard_files", f"{dname}.json")
+            rename = request.form.get('rename')
+            new_name = os.path.join("dashboard_files", f'{rename}.json')
+            if os.path.exists(dash_file):
+                os.rename(dash_file, new_name)
+                return redirect ('/dashboard')
+
+
 @app.route("/dashboard", methods=["GET", "POST"])
 def home():
     """
